ba7bad0a43a923cb485a1e4e097157f1dd5bf545
arasyidi <amri.geodesy@gmail.com>
2026-04-13 22:51:56 +0700

    sitemap, mdx img optimization, shared layout.astro

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://amrirasyidi.github.io',
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
