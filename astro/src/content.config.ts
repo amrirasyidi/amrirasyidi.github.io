d1b94dabd1a5debf1911d6c499dcc7071465e0e1
arasyidi <amri.geodesy@gmail.com>
2026-04-13 09:37:54 +0700

    Squash merge image_bloglist into rework

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -7,6 +7,7 @@ const blog = defineCollection({
     title: z.string(),
     date: z.coerce.date(),
     excerpt: z.string(),
+    image: z.string().optional(),
     tags: z.array(z.string()).default([]),
     draft: z.boolean().default(false),
   }),
